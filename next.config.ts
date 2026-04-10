bdabd8bfb000a2c7e43a6379ec9e3e68ce441566
Prashant Pandey <prashant.pandey@example.com>
2026-04-10 15:55:20 +0530

    Fix trailingSlash for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/CourierIQ",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
